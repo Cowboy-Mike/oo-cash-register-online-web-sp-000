8ca8a3c53a645bbd339313053d3cc04ec8be3d75
Mike Nanto <mike@thecowboymechanic.com>
2021-02-06 23:21:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,9 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
+  
+  def items(items)
+    @items = items
+  end
   
   def initialize(discount=0)
     @total = 0
